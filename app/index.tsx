969a320173dc2f64d48ab925a2551c345ef33756
Vinicios Neves <vinicios.neves@mac>
2025-01-07 18:32:19 +0000

    video 2.2

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,12 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View
       style={styles.background}
     >
-      <Text style={styles.text}>Edit app/index.tsx to edit this screen.</Text>
+      <Image source={require('./pomodoro.png')}/>
+      <View style={styles.actions}/>
     </View>
   );
 }
@@ -15,9 +16,16 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: '#01080E'
+    backgroundColor: '#01080E',
+    gap: 40
   },
-  text: {
-    color: '#fff'
+  actions: {
+    paddingHorizontal: 20,
+    paddingVertical: 24,
+    backgroundColor: '#14448080',
+    width: '80%',
+    borderRadius: 32,
+    borderWidth: 2,
+    borderColor: '#144480'
   }
 })
